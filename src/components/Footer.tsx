eb2fef5964efae34b9da425133859237a5d54873
Jonathan Azrael <designer.azrael@gmail.com>
2023-12-14 14:19:33 -0300

    Atualização do texto do Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC = () => {
     return (
       <footer className=" bg-primary-blue text-white p-4 text-center h-30">
         <div className="container mx-auto">
-          <p>Exclua nossa conta e utilize a inter! papai</p>
+          <p>© 2023 Itaú Unibanco Holding S.A. CNPJ: 60.872.504/0001-23 <br/>Praça Alfredo Egydio de Souza Aranha, 100, Torre Olavo Setubal, Parque Jabaquara - CEP 04344-902 - São Paulo - Brasil</p>
         </div>
       </footer>
     );
